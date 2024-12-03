8b8319c3439600edab99e4fa5ed48947c4fd997c
nafisa492003 <afifayasmin2003@gmail.com>
2024-12-03 12:59:36 +0600

    fixing routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import Reg_from from './Components/Reg_from'
-// import Log_in from './Components/Log_in'
+import Log_in from './Components/Log_in'
 const App = () => {
-  return (
-    <>
-   <Reg_from/>
-   {/* <Log_in/> */}
-    </>
-  )
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+          <Route path="/" element={<Reg_from />} />
+          <Route path="/login" element={<Log_in />} />
+        </Route>
+    )
+  );
+  return <RouterProvider router={router} />;
+  
 }
 
 export default App
--- src/Components/Reg_from.jsx
+++ src/Components/Reg_from.jsx
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 import img from "../assets/reg_pick.png";
 import { FaEye, FaEyeSlash } from "react-icons/fa6";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { Audio } from "react-loader-spinner";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const Reg_from = () => {
   const auth = getAuth();
+  const navigate = useNavigate();
   let [email, setEmail] = useState("");
   let [emailerr, setEmailerr] = useState(false);
   let [name, setName] = useState("");
@@ -37,9 +40,12 @@ const Reg_from = () => {
       setLoder(true);
 
       createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-          alert("success");
-        })
+        .then(() => {
+          toast("Registration successfully done");
+        setTimeout(() => {
+          navigate("/login");
+        }, 200);
+      })
         .catch((error) => {
           setLoder(false);
           console.log(error.code);
@@ -47,61 +53,77 @@ const Reg_from = () => {
             setEmailerr(true);
           }
         });
+        setEmail("");
+    setName("");
+    setPassword("");
     }
   };
   return (
-    <div className='flex'>
-      <div className='w-1/2 flex justify-end items-center mr-[69px]'>
+    <div className="flex">
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition:Bounce
+      />
+      <div className="w-1/2 flex justify-end items-center mr-[69px]">
         <div>
-          <h1 className='font-nunito font-bold text-[35px] text-primary mb-3'>
+          <h1 className="font-nunito font-bold text-[35px] text-primary mb-3">
             Get started with easily register
           </h1>
-          <p className='font-nunito font-normal text-[21px] text-primary'>
+          <p className="font-nunito font-normal text-[21px] text-primary">
             Free register and you can enjoy it
           </p>
-          <form action=''>
-            <div className=' relative mt-[42px]'>
-              <label className='font-nunito font-semibold text-[13px] text-primary bg-white px-4 absolute top-[-8px] left-[40px]'>
+          <form action="">
+            <div className=" relative mt-[42px]">
+              <label className="font-nunito font-semibold text-[13px] text-primary bg-white px-4 absolute top-[-8px] left-[40px]">
                 Email Address
               </label>
               <input
-                className='w-[368px] h-[81px] rounded-lg  border-[1.72px] border-bad font-nunito font-semibold text-[21px] text-primary px-[52px] py-[26px] outline-none'
+                className="w-[368px] h-[81px] rounded-lg  border-[1.72px] border-bad font-nunito font-semibold text-[21px] text-primary px-[52px] py-[26px] outline-none"
                 onChange={(e) => {
                   setEmail(e.target.value);
                   setEmailerr(false);
                 }}
-                type='text'
+                type="text"
               />
               {emailerr && (
                 <>
-                  <p className='text-red-600'>Please enter valid email</p>
+                  <p className="text-red-600">Please enter valid email</p>
                 </>
               )}
             </div>
-            <div className=' relative mt-[42px]'>
-              <label className='font-nunito font-semibold text-[13px] text-primary bg-white px-4 absolute top-[-8px] left-[40px]'>
+            <div className=" relative mt-[42px]">
+              <label className="font-nunito font-semibold text-[13px] text-primary bg-white px-4 absolute top-[-8px] left-[40px]">
                 Full name
               </label>
               <input
-                className='w-[368px] h-[81px] rounded-lg  border-[1.72px] border-bad font-nunito font-semibold text-[21px] text-primary px-[52px] py-[26px] outline-none'
+                className="w-[368px] h-[81px] rounded-lg  border-[1.72px] border-bad font-nunito font-semibold text-[21px] text-primary px-[52px] py-[26px] outline-none"
                 onChange={(e) => {
                   setName(e.target.value);
                   setNameerr(false);
                 }}
-                type='text'
+                type="text"
               />
               {nameerr && (
                 <>
-                  <p className='text-red-600'>Name field is empty</p>
+                  <p className="text-red-600">Name field is empty</p>
                 </>
               )}
             </div>
-            <div className=' relative mt-[42px]'>
-              <label className='font-nunito font-semibold text-[13px] text-primary bg-white px-4 absolute top-[-8px] left-[40px]'>
+            <div className=" relative mt-[42px]">
+              <label className="font-nunito font-semibold text-[13px] text-primary bg-white px-4 absolute top-[-8px] left-[40px]">
                 Password
               </label>
               <input
-                className='w-[368px] h-[81px] rounded-lg  border-[1.72px] border-bad font-nunito font-semibold text-[21px] text-primary px-[52px] py-[26px] outline-none'
+                className="w-[368px] h-[81px] rounded-lg  border-[1.72px] border-bad font-nunito font-semibold text-[21px] text-primary px-[52px] py-[26px] outline-none"
                 onChange={(e) => {
                   setPassword(e.target.value);
                   setPassworderr(false);
@@ -112,57 +134,45 @@ const Reg_from = () => {
                 <FaEyeSlash
                   onClick={handleToggle}
                   size={25}
-                  className='absolute top-[28px] right-[156px] cursor-pointer'
+                  className="absolute top-[28px] right-[156px] cursor-pointer"
                 />
               ) : (
                 <FaEye
                   onClick={handleToggle}
                   size={25}
-                  className='absolute top-[28px] right-[156px] cursor-pointer'
+                  className="absolute top-[28px] right-[156px] cursor-pointer"
                 />
               )}
               {passworderr && (
                 <>
-                  <p className='text-red-600'>Password field is empty</p>
+                  <p className="text-red-600">Password field is empty</p>
                 </>
               )}
             </div>
             {loder ? (
-              <div className='w-[360px]  flex justify-center'>
-                <Audio
-                  height='80'
-                  width='80'
-                  radius='9'
-                  color='green'
-                  ariaLabel='loading'
-                  wrapperStyle
-                  wrapperClass
-                />
+              <div className="w-[360px]  flex justify-center">
+               
               </div>
             ) : (
               <button
                 onClick={handleSubmit}
-                className='font-nunito font-semibold text-[21px] text-white bg-secondary px-[147px] py-[20px] rounded-[86px] mt-[40px]'
+                className="font-nunito font-semibold text-[21px] text-white bg-secondary px-[147px] py-[20px] rounded-[86px] mt-[40px]"
               >
                 {" "}
                 Sign up
               </button>
             )}
-            <p className='font-openSans font-normal text-[13px] text-dark mt-[20px] ps-[75px]'>
+            <p className="font-openSans font-normal text-[13px] text-dark mt-[20px] ps-[75px]">
               Already have an account ?{" "}
-              <a
-                className=' font-bold
-               text-orange'
-                href=''
-              >
+              <a className="font-bold text-orange" href="">
                 Sign In
               </a>
             </p>
           </form>
         </div>
       </div>
-      <div className='w-1/2'>
-        <img className='w-full h-screen object-cover' src={img} alt='' />
+      <div className="w-1/2">
+        <img className="object-cover w-full h-screen" src={img} alt="" />
       </div>
     </div>
   );
